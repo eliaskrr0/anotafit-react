4882ce6bd8d7aa2ed3e799afb5e0ffdfee114d00
Ilyasse <developer.elias.essadak@gmail.com>
2024-12-06 23:56:39 +0100

    se agrega llamada para obtener los datos de la tabla de rutina semanal

--- src/vista/componentes/TablaRutinaSemanal.js
+++ src/vista/componentes/TablaRutinaSemanal.js
@@ -1,11 +1,25 @@
+import React, { useEffect, useState } from "react"; import axios from "axios"
+
 const TablaRutinaSemanal = () => {
+    const [ejercicios, setEjercicios] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/anotafit/ejercicios/todos")
+            .then(response => {
+                setEjercicios(response.data)
+            })
+            .catch(error => {
+                console.error("Hubo un error al obtener los datos de los ejercicios.")
+            });
+    }, []);
+
     return (
         <div className="InicioVista">
             <h2>Rutina semanal</h2>
             <table className='table table-bordered table-dark'>
                 <thead>
                     <tr>
-                        <th>Fecha</th>
+                        <th>Fecha cambio</th>
                         <th>Tipo de ejercicio</th>
                         <th>Ejercicio</th>
                         <th>Peso adicional</th>
@@ -14,14 +28,16 @@ const TablaRutinaSemanal = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>06/12/2024</td>
-                        <td>Tiron</td>
-                        <td>Dominadas</td>
-                        <td>25kg</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
+                    {ejercicios.map(ejercicio => (
+                        <tr key={ejercicio.idEjercicio}>
+                            <td>{ejercicio.fechaModificacion}</td>
+                            <td>{ejercicio.tipoEjercicio}</td>
+                            <td>{ejercicio.nombre}</td>
+                            <td>{ejercicio.pesoAdicional} kg</td>
+                            <td>{ejercicio.series}</td>
+                            <td>{ejercicio.repeticiones}</td>
+                        </tr>
+                    ))}                    
                 </tbody>
             </table>
         </div>
